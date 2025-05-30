11cf68ca7fd7600a3874dca710fc24aaed542216
Jonathan Edgar Walter Dawsa <125462998+Dawsatek22@users.noreply.github.com>
2025-05-30 10:11:54 +0200

    Add files via upload

---
+++ eng/orp_joybot.eng/build/CMakeFiles/dctest1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dctest1.dir/src/dctest1.cpp.o"
+  "CMakeFiles/dctest1.dir/src/dctest1.cpp.o.d"
+  "dctest1"
+  "dctest1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dctest1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ eng/orp_joybot.eng/build/CMakeFiles/joytest.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for joytest.
+# This may be replaced when dependencies are built.
